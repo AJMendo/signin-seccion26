d29b21eb61525368f6343b775c05bff17a8b4827
Antonio José Mendoza <mendojimenez@gmail.com>
2024-06-14 17:11:49 +0200

    SignIn Flutter

--- lib/services/google_signin_service.dart
+++ lib/services/google_signin_service.dart
@@ -22,6 +22,18 @@ static Future <GoogleSignInAccount?> signInWithGoogle() async {
     print( googleKey?.idToken );
     // TODO:  Llamar un servicio REST a nuestro backend
     // con el ID Token
+    print('ID TOKEN');
+    String? token = googleKey?.idToken;
+    if (token != null) {
+        while (token!.length > 0) {
+          int initLength = (token!.length >= 500 ? 500 : token.length);
+          print(token.substring(0, initLength));
+          int endLength = token.length;
+          token = token.substring(initLength, endLength);
+        }
+      } else {
+        print('No se pudo obtener el token.');
+      }
 
     return account;
   } catch (e) {
